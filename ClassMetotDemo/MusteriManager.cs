fed827591720879892d8af510619f6241f586dae
Ahmet Yalçın <ahmetyalcini@users.noreply.github.com>
2021-01-20 11:53:00 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void AddCustomer(Musteri customer)
+        {
+            Console.WriteLine("Müşteri eklendi! " + customer.Ad + " "+ customer.Soyad);
+            Console.WriteLine();
+        }
+        public void DeleteCustomer(Musteri customer)
+        {
+            Console.WriteLine("Müşteri silindi! " + customer.Ad + " " + customer.Soyad);
+            Console.WriteLine();
+        }
+
+        
+
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri customer1 = new Musteri()
+            {
+                Id = 100,
+                Ad = "Alexi",
+                Soyad = "Laiho",
+                Yas = 41,
+                Cinsiyet = "Erkek",
+                Hometown = "Helsinki - Finland"
+            };
+
+            Musteri customer2 = new Musteri()
+            {
+                Id = 99,
+                Ad = "Gene",
+                Soyad = "Haas",
+                Yas = 75,
+                Cinsiyet = "Erkek",
+                Hometown = "Ohio - USA"
+            };
+
+            Musteri[] customers = new Musteri[]
+        {
+            customer1, customer2
+        };
+            foreach (var Musteri in customers)
+            {
+                Console.WriteLine("Ad: " +Musteri.Ad);
+                Console.WriteLine("Soyad: "  +Musteri.Soyad);
+                Console.WriteLine("Yaş: " +Musteri.Yas);
+                Console.WriteLine("Cinsiyet: " +Musteri.Cinsiyet);
+                Console.WriteLine("Şehir: " +Musteri.Hometown);
+                Console.WriteLine();
+            }
+
+            MusteriManager manager = new MusteriManager();
+            manager.AddCustomer(customer1);
+            manager.AddCustomer(customer2);
+            manager.DeleteCustomer(customer2);
+            
+        }
+    }
+
+    
+}
